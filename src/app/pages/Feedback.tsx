c3dd37d338e1b99f1919568dd13eac74c9889a5f
aadiityaasingh <adityasingh.6488@gmail.com>
2026-03-29 15:26:18 +0530

    updated the URl

--- src/app/pages/Feedback.tsx
+++ src/app/pages/Feedback.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useApp } from '../context/AppContext';
 import { toast } from '../components/Toast';
 // import BASE_URL from '../../config/api';
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://makers-kid-backend.onrender.com";
 
 export default function Feedback() {
   const { state } = useApp();
