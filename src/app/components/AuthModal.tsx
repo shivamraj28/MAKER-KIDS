7c33714d9d309c9fe472bcd8091d25e56e6d9d13
aadiityaasingh <adityasingh.6488@gmail.com>
2026-03-29 15:01:39 +0530

    changes

--- src/app/components/AuthModal.tsx
+++ src/app/components/AuthModal.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useApp } from '../context/AppContext';
 import { toast } from './Toast';
 // import BASE_URL from '../../config/api';
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://makers-kid-backend.onrender.com";
 import { useNavigate } from "react-router";
 
 interface AuthModalProps {
